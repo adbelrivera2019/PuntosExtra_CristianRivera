9fff589e1672f4d4d24b186ba70d01a2b4ca4db7
Cristian Rivera <adbel.rivera@unitec.edu>
2023-03-19 22:53:26 -0600

    comit

---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-ea03d',
+    appId: '1:609608091262:web:44a6eefdb251f7b7b1fa2f',
+    storageBucket: 'kanbanfire-ea03d.appspot.com',
+    apiKey: 'AIzaSyDaNnvLbqqLxJxmEQ7nR3VbM4GlbBIu27o',
+    authDomain: 'kanbanfire-ea03d.firebaseapp.com',
+    messagingSenderId: '609608091262',
+    measurementId: 'G-ZV8G8PZE2K',
+  },};
